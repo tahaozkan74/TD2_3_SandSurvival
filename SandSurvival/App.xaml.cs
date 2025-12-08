60fff1c7cd17796f0d6088ade0c93a92bd51f576
Fatih746 <fatih.yildirim@etu.univ-smb.fr>
2025-12-08 17:32:36 +0100

    Ajoutez des fichiers projet.

---
+++ SandSurvival/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SandSurvival
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
