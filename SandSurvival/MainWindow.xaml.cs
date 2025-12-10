7ac63693a7f1de9db990176968c3366e24d19ff3
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2025-12-10 11:36:05 +0100

    ajout image d'accueil

--- SandSurvival/MainWindow.xaml.cs
+++ SandSurvival/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace SandSurvival
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
